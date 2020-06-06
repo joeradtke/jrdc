6ea9e5b4cf6cf575b0abc6c6311b8698746f0673
joeradtke <joeradtke@yahoo.com>
2020-06-06 09:29:01 -0600

    added bootstrap jquery and jquery-ui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,21 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-
+// jquery
+import $ from 'jquery'
+global.$ = $
+global.jQuery = $
+require('jquery-ui');
+// jquery-ui theme
+require.context('file-loader?name=[path][name].[ext]&context=node_modules/jquery-ui-dist!jquery-ui-dist', true,    /jquery-ui\.css/ );
+require.context('file-loader?name=[path][name].[ext]&context=node_modules/jquery-ui-dist!jquery-ui-dist', true,    /jquery-ui\.theme\.css/ );
+// bootstrap
+import 'bootstrap/dist/js/bootstrap';
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//css
+import './stylesheets/application';
